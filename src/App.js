168e9fe654ec0a5963d47b7646e381cdc2e1c19b
FendadoBikini0 <araujobrito69@gmail.com>
2025-08-12 11:03:06 -0300

    Correção about

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Catalog from './components/Catalog';
 import Portfolio from './components/Portfolio';
+import About from './components/About';
 
 function App() {
   return (
